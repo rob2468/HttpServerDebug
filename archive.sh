828a84901d4dd67e33a67f56587df936bb3364d9
jam.chenjun <jam.chenjun@gmail.com>
2017-08-13 21:02:13 +0800

    打包脚本支持指定输出静态库中是否包含依赖的第三方库

--- archive.sh
+++ archive.sh
@@ -1,23 +1,42 @@
 #!/usr/bin/env bash
+
+# Dependencies onfiguration
+FMDB_INCLUDE=0
+
+# Constant variables
 PROJECT_NAME="HttpServerDebug"
 OUTPUT_FOLDER_NAME="output"
 BUILD_FOLDER_NAME="build"
-IPHONEOS_FOLDER_NAME="Debug-iphoneos"
-IPHONESIMULATOR_FOLDER_NAME="Debug-iphonesimulator"
+IPHONEOS_SDK="iphoneos"
+IPHONESIMULATOR_SDK="iphonesimulator"
 LIBRARY_NAME="libHttpServerDebug.a"
 
 rm -rf ${OUTPUT_FOLDER_NAME}
 mkdir ${OUTPUT_FOLDER_NAME}
 
 # Build Device and Simulator versions
-xcodebuild -project "${PROJECT_NAME}.xcodeproj" -target "FMDB" -target "HttpServerDebug" -configuration "Debug" -sdk iphoneos ONLY_ACTIVE_ARCH=NO
-xcodebuild -project "${PROJECT_NAME}.xcodeproj" -target "FMDB" -target "HttpServerDebug" -configuration "Debug" -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+build_combine() {
+  SDK=$1
+  build_cmd='xcodebuild -project "${PROJECT_NAME}.xcodeproj" -configuration "Debug" -sdk ${SDK} ONLY_ACTIVE_ARCH=NO'
+  combine_cmd='libtool -static -o "${BUILD_FOLDER_NAME}/Debug-${SDK}/${LIBRARY_NAME}" "${BUILD_FOLDER_NAME}/Debug-${SDK}/${LIBRARY_NAME}"'
+
+  eval ${build_cmd}' -target "HttpServerDebug"'
+  if [[ FMDB_INCLUDE -eq 1 ]]; then
+    eval ${build_cmd}' -target "FMDB"'
+    combine_cmd=${combine_cmd}' "${BUILD_FOLDER_NAME}/Debug-${SDK}/libFMDB.a"'
+  fi
+
+  eval ${combine_cmd}
+}
+
+build_combine ${IPHONEOS_SDK}
+build_combine ${IPHONESIMULATOR_SDK}
 
 # Create universal binary file
-lipo -create -output "${OUTPUT_FOLDER_NAME}/${LIBRARY_NAME}" "${BUILD_FOLDER_NAME}/${IPHONEOS_FOLDER_NAME}/${LIBRARY_NAME}" "${BUILD_FOLDER_NAME}/${IPHONESIMULATOR_FOLDER_NAME}/${LIBRARY_NAME}"
+lipo -create -output "${OUTPUT_FOLDER_NAME}/${LIBRARY_NAME}" "${BUILD_FOLDER_NAME}/Debug-${IPHONEOS_SDK}/${LIBRARY_NAME}" "${BUILD_FOLDER_NAME}/Debug-${IPHONESIMULATOR_SDK}/${LIBRARY_NAME}"
 
 # Copy header files
-cp -R "${BUILD_FOLDER_NAME}/${IPHONEOS_FOLDER_NAME}/include/" "${OUTPUT_FOLDER_NAME}/Headers/"
+cp -R "${BUILD_FOLDER_NAME}/Debug-${IPHONEOS_SDK}/include/" "${OUTPUT_FOLDER_NAME}/Headers/"
 
 # Copy bundle
 cp -R "${PROJECT_NAME}/Resources/${PROJECT_NAME}.bundle" "${OUTPUT_FOLDER_NAME}/"
