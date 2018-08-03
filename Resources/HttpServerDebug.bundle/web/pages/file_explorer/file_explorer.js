2b4f2db5108a37671c04b21910acd2239e77e9e0
jam.chenjun <jam.chenjun@gmail.com>
2018-08-03 15:15:58 +0800

    file explorer, fix bug, column can't resize ; isolate resizing js codes

--- Resources/HttpServerDebug.bundle/web/pages/file_explorer/file_explorer.js
+++ Resources/HttpServerDebug.bundle/web/pages/file_explorer/file_explorer.js
@@ -188,7 +188,7 @@ function constructDirectoryHTML(dirData, dirIdx) {
     // split handler view
     var splitHandlerEle = document.createElement('div');
     splitHandlerEle.setAttribute('class', 'split-handler');
-    splitHandlerEle.addEventListener('onmousedown', function (event) {
+    splitHandlerEle.addEventListener('mousedown', function (event) {
         initResizeDrag(event);
     });
     dirContainerEle.appendChild(splitHandlerEle);
@@ -317,45 +317,6 @@ function removeNoNeededDataAndViews(section, row) {
     for (var i = dirContainers.length - 1; i >= section + 1; i--) {
         dirContainers[i].remove();
     }
-}
-
-/* resize column functions */
-var startResizeX;
-var startResizeWidth;
-var targetResizeEle;
-
-function initResizeDrag(event) {
-    targetResizeEle = event.currentTarget.parentElement;
-
-    // initial value
-    startResizeX = event.clientX;
-    startResizeWidth = parseInt(document.defaultView.getComputedStyle(targetResizeEle).width, 10);
-
-    document.documentElement.addEventListener('mousemove', doResizeDrag, false);
-    document.documentElement.addEventListener('mouseup', stopResizeDrag, false);
-}
-
-function doResizeDrag(event) {
-    // calculate target element width and modify views
-    var sidebarEle = document.getElementById('property-sidebar');
-    if (targetResizeEle === sidebarEle) {
-        var widthStr = (startResizeWidth - event.clientX + startResizeX) + 'px';
-        var fileExpEle = document.getElementById('file-explorer');
-
-        targetResizeEle.style.width = widthStr;
-        fileExpEle.style.right = widthStr;
-    } else {
-        targetResizeEle.style.width = (startResizeWidth + event.clientX - startResizeX) + 'px';
-    }
-
-    if (event.preventDefault) {
-        event.preventDefault();
-    }
-}
-
-function stopResizeDrag(event) {
-    document.documentElement.removeEventListener('mousemove', doResizeDrag, false);
-    document.documentElement.removeEventListener('mouseup', stopResizeDrag, false);
 }
 
 /**
---
+++ Resources/HttpServerDebug.bundle/web/pages/file_explorer/file_explorer_resize.js
@@ -0,0 +1,38 @@
+/* resize column functions */
+var startResizeX;
+var startResizeWidth;
+var targetResizeEle;
+
+function initResizeDrag(event) {
+    targetResizeEle = event.currentTarget.parentElement;
+
+    // initial value
+    startResizeX = event.clientX;
+    startResizeWidth = parseInt(document.defaultView.getComputedStyle(targetResizeEle).width, 10);
+
+    document.documentElement.addEventListener('mousemove', doResizeDrag, false);
+    document.documentElement.addEventListener('mouseup', stopResizeDrag, false);
+}
+
+function doResizeDrag(event) {
+    // calculate target element width and modify views
+    var sidebarEle = document.getElementById('property-sidebar');
+    if (targetResizeEle === sidebarEle) {
+        var widthStr = (startResizeWidth - event.clientX + startResizeX) + 'px';
+        var fileExpEle = document.getElementById('file-explorer');
+
+        targetResizeEle.style.width = widthStr;
+        fileExpEle.style.right = widthStr;
+    } else {
+        targetResizeEle.style.width = (startResizeWidth + event.clientX - startResizeX) + 'px';
+    }
+
+    if (event.preventDefault) {
+        event.preventDefault();
+    }
+}
+
+function stopResizeDrag(event) {
+    document.documentElement.removeEventListener('mousemove', doResizeDrag, false);
+    document.documentElement.removeEventListener('mouseup', stopResizeDrag, false);
+}
